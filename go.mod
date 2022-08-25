834f65716dfd5de42a3dcaf02e9e2c9299c7efcf
Maciej Lewicki <maciej.lewicki@toggl.com>
2022-08-24 23:17:26 -0400

    add liquid generating from string

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mlewicki12/liquid-generator
+
+go 1.19
+
+require (
+	github.com/osteele/liquid v1.3.0 // indirect
+	github.com/osteele/tuesday v1.0.3 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- liquid-generator.go
+++ liquid-generator.go
@@ -1,7 +1,29 @@
 package liquidgenerator
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	liquid "github.com/osteele/liquid"
+)
 
 func Print() {
 	fmt.Printf("hello world\n")
+}
+
+func GenerateTemplate() {
+	engine := liquid.NewEngine()
+	template := `<h1>{{ page.title }}</h1>`
+	bindings := map[string]interface{}{
+		"page": map[string]interface{}{
+			"title": "Introduction",
+		},
+	}
+
+	out, err := engine.ParseAndRenderString(template, bindings)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(out)
 }
